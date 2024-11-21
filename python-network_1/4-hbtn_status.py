2ab539b851a2426942a29288e4142157338e2c13
d-k0de <d.ifechukwu@alustudent.com>
2024-11-21 15:02:00 +0200

    Updated task 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -4,8 +4,15 @@ Python script that fetches an URL with requests package
 """
 import requests
 
-
 if __name__ == "__main__":
-    r = requests.get('https://alu-intranet.hbtn.io/status')
-    t = r.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
+    url = 'https://alu-intranet.hbtn.io/status'
+    response = requests.get(url)
+
+    # Check if the response is successful
+    if response.status_code == 200:
+        print('Body response:\n\t- type: {}\n\t- content: {}'.format(
+            type(response.text), response.text
+        ))
+    else:
+        print(f"Error: Received status code {response.status_code}")
+
