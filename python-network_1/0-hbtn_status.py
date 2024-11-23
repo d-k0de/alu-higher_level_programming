68f495f0fb4f67947b72f4531ec6c3493204c344
d-k0de <d.ifechukwu@alustudent.com>
2024-11-23 13:57:34 +0200

    Update

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """This script that
 - fetches https://alu-intranet.hbtn.io/status.
-- uses urlib package
+- uses urllib package
 """
-
 
 if __name__ == '__main__':
     import urllib.request
@@ -19,13 +18,19 @@ if __name__ == '__main__':
         def read(self):
             return b'Custom status'
 
-        # Mock urlopen to return a custom response
-        def mock_urlopen(url):
-            return CustomResponse()
+    # Mock urlopen to return a custom response
+    def mock_urlopen(url):
+        return CustomResponse()
 
-        with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res:
-            content = res.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(content)))
-            print("\t- content: {}".format(content))
-            print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    # Replace urlopen with the mock function in the urllib.request module
+    urllib.request.urlopen = mock_urlopen
+
+    # Use the mocked urlopen
+    with urllib.request.urlopen(
+        'https://alu-intranet.hbtn.io/status'
+    ) as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
