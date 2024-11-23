563db94c1a7ffddf657725a634a58d4f0a1a05f0
d-k0de <d.ifechukwu@alustudent.com>
2024-11-23 12:37:37 +0200

    Update on task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
-"""__summary__
-- Write a Python script that fetches https://alu-intranet.hbtn.io/status
-- using the urllib package.
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
-import urllib.request
 
 
 if __name__ == '__main__':
-    # Create a request with a User-Agent header
-    req = urllib.request.Request(
-        'https://intranet.hbtn.io/status',
-        headers={'User -Agent': 'Mozilla/5.0'}
-        )
+    import urllib.request
 
-    try:
-        with urllib.request.urlopen(req) as response:
-            content = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(content)))
-            print("\t- content: {}".format(content))
-            print("\t- utf8 content: {}".format(content.decode("utf-8")))
-    except urllib.error.HTTPError as e:
-        print(f"HTTP Error: {e.code} - {e.reason}")
-    except urllib.error.URLError as e:
-        print(f"URL Error: {e.reason}")
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
