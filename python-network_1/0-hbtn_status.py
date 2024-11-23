656b3222a60afab0c90a9b7c9cb1141a1cc37716
d-k0de <d.ifechukwu@alustudent.com>
2024-11-23 12:28:05 +0200

    Update on task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://alu-intranet.hbtn.io/status.
-- uses urlib package
+"""__summary__
+- Write a Python script that fetches https://alu-intranet.hbtn.io/status
+- using the urllib package.
 """
+import urllib.request
 
 
 if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res:
-        content = res.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
