b3b51d70eedfad61db066afbf857b12868136908
d-k0de <d.ifechukwu@alustudent.com>
2024-11-26 10:36:37 +0200

    tasks 0 and 4 updates

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,36 +1,13 @@
 #!/usr/bin/python3
-"""This script that
-- fetches https://alu-intranet.hbtn.io/status.
-- uses urllib package
 """
+Python script that takes in a URL, sends a request and displays
+the value of the X-Request-Id variable found in the header
+"""
+import urllib.request
+import sys
 
-if __name__ == '__main__':
-    import urllib.request
 
-    # Custom mock response class
-    class CustomResponse:
-        def __enter__(self):
-            return self
-
-        def __exit__(self, exc_type, exc_val, exc_tb):
-            pass
-
-        def read(self):
-            return b'Custom status'
-
-    # Mock urlopen to return a custom response
-    def mock_urlopen(url):
-        return CustomResponse()
-
-    # Replace urlopen with the mock function in the urllib.request module
-    urllib.request.urlopen = mock_urlopen
-
-    # Use the mocked urlopen
-    with urllib.request.urlopen(
-        'https://alu-intranet.hbtn.io/status'
-    ) as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        print(html.get('X-Request-Id'))
